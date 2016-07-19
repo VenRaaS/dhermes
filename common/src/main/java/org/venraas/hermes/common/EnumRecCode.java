58c88babdb05548ea60e5796ed771ca86ec396c6
elk <elk@es2x-node-01.c.venraas-test.internal>
2016-07-19 03:53:35 +0000

    [common] enum for RecCode & RecType

---
+++ common/src/main/java/org/venraas/hermes/common/EnumRecCode.java
@@ -0,0 +1,14 @@
+package org.venraas.hermes.common;
+
+public enum EnumRecCode {
+	// -- https://github.com/VenRaaS/rec-api/blob/master/README.md#rec_type--rec_code
+	ClickStream_COOC,
+	
+	ClickStream_COOCM,
+	
+	ClickStreamInCategCode_COOC,
+
+	AlsoView_COOC,
+
+	Group_FavorCateg
+}
---
+++ common/src/main/java/org/venraas/hermes/common/EnumRecType.java
@@ -0,0 +1,11 @@
+package org.venraas.hermes.common;
+
+public enum EnumRecType {
+	// -- https://github.com/VenRaaS/rec-api/blob/master/README.md#rec_type--rec_code
+
+	ClickStream,
+	
+	AlsoView,
+	
+	Group
+}
