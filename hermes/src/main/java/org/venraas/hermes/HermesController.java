6ff7c5a38d807cd19e52a3f4127120a3cc51e618
elk <elk@es2x-node-01.c.venraas-test.internal>
2016-07-19 04:00:10 +0000

    []

--- hermes/src/main/java/org/venraas/hermes/HermesController.java
+++ hermes/src/main/java/org/venraas/hermes/HermesController.java
@@ -1,11 +1,20 @@
 package org.venraas.hermes;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.venraas.hermes.common.option.RecOption;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 
 
 @RestController
@@ -16,9 +25,30 @@ public class HermesController {
 	
 	@CrossOrigin
 	@RequestMapping(value = "/goods/rank", method = RequestMethod.GET)
-	public Object getGoodsRank_GET()
+	public Object get_goods_rank_GET(RecOption opt) {		
+		Calendar c = Calendar.getInstance();
+//TODO... check conf/
+		int h = c.get(Calendar.HOUR_OF_DAY);
+		
+		String clientID = String.format("%s_%s_%s", opt.getToken(), opt.getVen_guid(), opt.getVen_session());
+		HashFunction hf = Hashing.murmur3_128();
+		HashCode hc = hf.newHasher()
+		       .putString(clientID, Charsets.US_ASCII)
+		       .putInt(h)
+		       .hash();
+		Long l = hc.asLong();
+//TODO... numGrps		
+		Long grp_i= Math.abs(l % 5);		
+ 
+		return grp_i;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public Object hello()
 	{
 		return "hello world";
 	}
+	
 
 }
