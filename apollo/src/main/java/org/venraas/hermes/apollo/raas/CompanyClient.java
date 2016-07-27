19ab6af15162ead26b786bdef999fb649bc9903b
eric0329 <asdguo@mail2000.com.tw>
2016-07-26 21:39:09 -0700

    [apollo] migrate from javax to GSON

--- apollo/src/main/java/org/venraas/hermes/apollo/raas/CompanyClient.java
+++ apollo/src/main/java/org/venraas/hermes/apollo/raas/CompanyClient.java
@@ -1,11 +1,5 @@
 package org.venraas.hermes.apollo.raas;
 
-import java.io.StringReader;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonValue;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -14,15 +8,20 @@ import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.venraas.hermes.apollo.Apollo;
-import org.venraas.hermes.apollo.mappings.EnumCompany;
+import org.venraas.hermes.apollo.mappings.Com_pkgs;
 import org.venraas.hermes.common.Utility;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 //@Service
 public class CompanyClient {
 	
 	private static final String VENRAAS_INDEX_NAME = "venraas";
 	
-	static private final String TYPE_NAME = "com_pkgs_test"; 
+	static private final String TYPE_NAME = "com_pkgs"; 
 	
 	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(CompanyClient.class);	
 	
@@ -53,27 +52,31 @@ public class CompanyClient {
 	        SearchResponse resp = _apo.esClient().prepareSearch(VENRAAS_INDEX_NAME)
 	                .setTypes(TYPE_NAME)
 	                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-	                .setQuery(QueryBuilders.termQuery(EnumCompany.token.name(), token))	                	                
-	                .addSort(EnumCompany.webServerTime.name(), SortOrder.DESC)
+	                .setQuery(QueryBuilders.termQuery(Com_pkgs.token, token))	                	                
+	                .addSort(Com_pkgs.webServerTime, SortOrder.DESC)
+	                .setSize(1)
 	                .execute()
 	                .actionGet();
 	        	        
 	        if (0 < resp.getHits().getTotalHits()) {
 	        	SearchHit h = resp.getHits().getAt(0);	        		        	
 	        	String jsonStr = h.getSourceAsString();
-	        	JsonReader r = Json.createReader(new StringReader(jsonStr));
-	        	JsonArray comps = r.readObject().getJsonArray("companies");	        	
-	        	for (JsonValue v : comps) {
+	        	
+	        	JsonParser jsonParser = new JsonParser();
+	        	JsonObject o = jsonParser.parse(jsonStr).getAsJsonObject();	        	
+	        	JsonArray comps = o.getAsJsonArray("companies");
+	        		        	
+	        	for (JsonElement v : comps) {
 	        		JsonObject c = (JsonObject)v;
-	        		String tok = c.getString(EnumCompany.token.name());
+	        		String tok = c.get("token").getAsString();
 	        		
 	        		if (0 == tok.compareToIgnoreCase(token)) {
-	        			codeName = c.getString(EnumCompany.codeName.name());
+	        			codeName = c.get("codeName").getAsString();
 	        			break;
 	        		}
-	        	}
-	        	
+	        	}	        	
 	        }
+	        
 		} catch(Exception ex) {
 			VEN_LOGGER.error(Utility.stackTrace2string(ex));
 			VEN_LOGGER.error(ex.getMessage());
