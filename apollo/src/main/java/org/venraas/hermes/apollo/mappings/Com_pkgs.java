26b8eb47e854d8346653338ab8681f98c3d43f43
eric0329 <asdguo@mail2000.com.tw>
2016-07-26 21:37:06 -0700

    [apollo] migrate mapping field for nested query

---
+++ apollo/src/main/java/org/venraas/hermes/apollo/mappings/Com_pkgs.java
@@ -0,0 +1,17 @@
+package org.venraas.hermes.apollo.mappings;
+
+public class Com_pkgs {
+	
+	public static String webServerTime = "webServerTime"; 
+	
+	public static String token = "companies.token";
+	
+	public static String codeName = "companies.codeName";
+	
+	public static String domainName = "companies.domainName"; 
+	
+	public static String companyName = "companies.companyName";
+	
+	public static String companyEnabled = "companies.companyEnabled";
+
+}
