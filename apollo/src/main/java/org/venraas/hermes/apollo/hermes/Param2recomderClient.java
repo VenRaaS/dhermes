ba1cb1ec54f3baf4b6d649a79d29b9d63efb66d9
eric0329 <asdguo@mail2000.com.tw>
2016-07-28 02:25:45 -0700

    [apollo] - add function getDistinctGroups(), get Groups from ES

--- apollo/src/main/java/org/venraas/hermes/apollo/hermes/Param2recomderClient.java
+++ apollo/src/main/java/org/venraas/hermes/apollo/hermes/Param2recomderClient.java
@@ -1,10 +1,17 @@
 package org.venraas.hermes.apollo.hermes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.venraas.hermes.apollo.Apollo;
@@ -30,7 +37,54 @@ public class Param2recomderClient {
 	}
 	
 //	@CacheEvict(value="cache_category", allEntries=true)
-	public void reset() {		
+	public void reset() { }
+	
+	public List<String> getDistinctGroups (String codeName) {
+		VEN_LOGGER.info("caching getGroups({})", codeName);
+
+		List<String> grps = new ArrayList<String> (20);
+		
+		if (codeName == null || codeName.isEmpty())	return grps;
+						
+		String indexName = String.format("%s_hermes", codeName);
+		String aggName = "group_key_count";
+
+		try {
+			
+			//-- availability = 1 
+			QueryBuilder qb = QueryBuilders.boolQuery().filter(QueryBuilders.termQuery(EnumParam2recomder.availability.name(), 1));
+			
+			//-- Terms Aggregation, https://www.elastic.co/guide/en/elasticsearch/client/java-api/current/_bucket_aggregations.html#java-aggs-bucket-terms
+			AggregationBuilder ab = AggregationBuilders.terms(aggName).field(EnumParam2recomder.group_key.name());
+
+			SearchRequestBuilder searchReq = 		
+				_apo.esClient()
+				.prepareSearch(indexName)
+				.setTypes(TYPE_NAME)
+				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+				.setQuery(qb)
+				.addAggregation(ab)
+				.setSize(0);
+
+			SearchResponse resp = searchReq.execute().actionGet();
+
+			Gson gson = new Gson();
+			
+			if (0 < resp.getHits().getTotalHits()) {
+				Terms agg = resp.getAggregations().get(aggName);
+				for (Terms.Bucket b : agg.getBuckets()) {
+					String k = b.getKeyAsString();
+//					long c = b.getDocCount();
+					grps.add(k);
+				}
+			}
+			
+		} catch (Exception ex) {
+			VEN_LOGGER.error(Utility.stackTrace2string(ex));
+			VEN_LOGGER.error(ex.getMessage());
+		}
+		
+		return grps;
 	}
 
 //	@Cacheable(value="cache_category", key="{#codeName, #categoryCode}")
