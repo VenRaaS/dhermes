5f8ae18fed8057d9c0b347d4499d73f8eb75d3fd
eric0329 <asdguo@mail2000.com.tw>
2016-07-28 02:28:41 -0700

    [common] define constants

--- common/src/main/java/org/venraas/hermes/common/Constant.java
+++ common/src/main/java/org/venraas/hermes/common/Constant.java
@@ -3,4 +3,8 @@ package org.venraas.hermes.common;
 public class Constant {
 	
 	public static final double TRAFFIC_PERCENT_NORMAL = 80.0;
+	
+	public static final int MAX_NUM_GROUPS = 100;
+	
+	public static final String NORMAL_GROUP_KEY = "normal";
 }
