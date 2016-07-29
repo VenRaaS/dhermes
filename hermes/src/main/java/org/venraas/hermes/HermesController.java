46e32c8551f817b41a3dd01014c6105bb1314ff2
eric0329 <asdguo@mail2000.com.tw>
2016-07-29 14:25:42 +0800

    [hermes] - program of delivering the income request to backend recAPI - add appach HttpClient lib

--- hermes/src/main/java/org/venraas/hermes/HermesController.java
+++ hermes/src/main/java/org/venraas/hermes/HermesController.java
@@ -1,9 +1,17 @@
 package org.venraas.hermes;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,6 +24,7 @@ import org.venraas.hermes.apollo.hermes.Param2recomderClient;
 import org.venraas.hermes.apollo.mappings.EnumParam2recomder;
 import org.venraas.hermes.apollo.raas.CompanyClient;
 import org.venraas.hermes.common.EnumOptionBase;
+import org.venraas.hermes.common.Utility;
 import org.venraas.hermes.common.option.RecOption;
 
 import com.google.gson.Gson;
@@ -44,8 +53,7 @@ public class HermesController {
 		GroupRoute gr = new GroupRoute();
 		String grpKey = gr.routing(codeName, clientID);
 		
-		Map<String, Object> mapping = null;
-		
+		Map<String, Object> mapping = null;		
 		Param2recomderClient p2r = new Param2recomderClient();
 		List<Map<String, Object>> maps = p2r.getGroupMapping(codeName, grpKey);		
 		for (Map<String, Object> m : maps) {			
@@ -70,8 +78,51 @@ public class HermesController {
 				break;
 			} 
 		}
+
+		HashMap<String, Object> outParamMap = new HashMap<String, Object> (paramMap);
+		String apiURL = (String) mapping.get(EnumParam2recomder.api_url.name());
+		List<String> fields = (List<String>) mapping.get(EnumParam2recomder.output_params.name());
+		for (String f : fields) {
+			String v = (String) mapping.get(f);
+			outParamMap.put(f, v);			
+		}
+		Gson g = new Gson();
+		String outParam = g.toJson(outParamMap);		
+		System.out.println(outParam);
+		
+		String resp = "";
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost post = new HttpPost(apiURL);
+		try {
+			StringEntity input = new StringEntity(outParam);
+			input.setContentType("application/json");
+			post.setEntity(input);
+			
+			HttpResponse response = httpClient.execute(post);
+
+			int httpStatusCode = response.getStatusLine().getStatusCode();
+			if (! String.valueOf(httpStatusCode).startsWith("2")) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = 
+					new BufferedReader(
+							new InputStreamReader(response.getEntity().getContent()));
+			
+			StringBuilder strBuilder = new StringBuilder();
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				strBuilder.append(line);
+			}
+			resp = strBuilder.toString();
+		}
+		catch (Exception ex) {
+			VEN_LOGGER.error(Utility.stackTrace2string(ex));
+			VEN_LOGGER.error(ex.getMessage());
+		}	
  
-		return mapping;
+		return resp;
 	}
 	
 	@CrossOrigin
