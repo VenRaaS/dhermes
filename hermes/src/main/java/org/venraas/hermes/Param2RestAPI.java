870bf8ab1301d5ae283b24eb11b47685aa0af5b6
eric0329 <asdguo@mail2000.com.tw>
2016-08-09 18:01:25 +0800

    [hermes] add class Param2RestAPI for searching the mapping according to input parameter

--- hermes/src/main/java/org/venraas/hermes/Param2RestAPI.java
+++ hermes/src/main/java/org/venraas/hermes/Param2RestAPI.java
@@ -1,5 +1,6 @@
 package org.venraas.hermes;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,34 +10,51 @@ import org.venraas.hermes.apollo.hermes.Param2recomderClient;
 import org.venraas.hermes.apollo.mappings.EnumParam2recomder;
 
 public class Param2RestAPI {
+	String _codeName;
+	String _grpKey;
+	Map<String, Object> _inParamMap;
+	Map<String, Object> _rsMap = new HashMap<String, Object>();
 	
 	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(Param2RestAPI.class);	
 	
 	private Param2RestAPI() { }
 
-	public Param2RestAPI getBuilder() {
+	public static Param2RestAPI getBuilder() {
 		return new Param2RestAPI();
 	}
-	
-	public Param2RestAPI set_codeName(String codeName) {	
-//TODO...		
+
+	public Param2RestAPI set_codeName(String codeName) {
+		this._codeName = codeName;
 		return this;
 	}
 	
-	public Map<String, Object> getMapping(String codeName, String grpKey, Map<String, Object> inParamMap) {
-		VEN_LOGGER.info("codeName: {}, grpKey: {}", codeName, grpKey);
+	public Param2RestAPI set_grpKey(String grpKey) {
+		this._grpKey = grpKey;
+		return this;
+	}
+	
+	public Param2RestAPI set_inParamMap(Map<String, Object> inParamMap) {
+		this._inParamMap = inParamMap;
+		return this;
+	}
+	
+	public Param2RestAPI build() {
 		
-		Map<String, Object> mapping = null;
+		VEN_LOGGER.info("codeName: {}, grpKey: {}", _codeName, _grpKey);
+		
+		Map<String, Object> _rsMap = null;
 
 		Param2recomderClient p2r = new Param2recomderClient();
-		List<Map<String, Object>> maps = p2r.getGroupMapping(codeName, grpKey);
+		
+		//-- list available mappings with specified GroupKey
+		List<Map<String, Object>> maps = p2r.getGroupMapping(_codeName, _grpKey);
 		
 		for (Map<String, Object> m : maps) {
 			boolean matchAllKeys = true;
 			
-			List<String> fields = (List<String>) m.get(EnumParam2recomder.keys2recomder.name());						
+			List<String> fields = (List<String>) m.get(EnumParam2recomder.in_keys2recomder.name());						
 			for (String f : fields) {
-				String inputV = (String) inParamMap.get(f);
+				String inputV = (String) _inParamMap.get(f);
 				String regV = (String) m.get(f);
 				
 				if (null == inputV || null == regV || 
@@ -49,12 +67,17 @@ public class Param2RestAPI {
 			}
 			
 			if (matchAllKeys) {
-				mapping = m;
+				_rsMap = m;
 				break;
-			} 
+			}
 		}
 		
-		return mapping;
+		return this;
 	}
+
+	public Map<String, Object> getMapping() {
+		return _rsMap;
+	}
+	
 	
 }
