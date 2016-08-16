77533584530d89a3251b03e60a1b58d2817ac406
eric0329 <asdguo@mail2000.com.tw>
2016-08-16 13:28:31 +0800

    [hermes] apply PoolingHttpClientConnectionManager for pooling of client connections which is able to service connection requests from multiple execution threads

--- hermes/src/main/java/org/venraas/hermes/APIConnector.java
+++ hermes/src/main/java/org/venraas/hermes/APIConnector.java
@@ -13,6 +13,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +22,15 @@ import org.venraas.hermes.common.Utility;
 
 public class APIConnector {
 	
-	static RequestConfig _reqConfig = null;
+	private static CloseableHttpClient _httpClient = null;
+	
+	private static RequestConfig _reqConfig = null;	
+	
+	private static final APIConnector _conn = new APIConnector();
 	
 	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(APIConnector.class);
-	
-	public APIConnector() {
+			
+	private APIConnector() {
 /*///
  		if (null == _httpClient) {
 			synchronized (APIConnector.class){
@@ -48,36 +53,60 @@ public class APIConnector {
 */
 	}
 	
+	public static APIConnector getInstance() {
+		
+		if (null == _httpClient) {
+			synchronized (APIConnector.class) {
+				if (null == _httpClient) { 
+					PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+				    cm.setMaxTotal(Constant.CONNECTION_POOL_MAX_TOTAL);
+				    
+				    _httpClient = HttpClients.custom()
+				    	.setConnectionManager(cm)
+				    	.setConnectionManagerShared(true)
+				    	.build();
+				}
+			}
+		}
+		
+		if (null == _reqConfig) {
+			synchronized (APIConnector.class) {
+				if (null == _reqConfig) {
+					_reqConfig = RequestConfig.custom()
+				    .setConnectionRequestTimeout(Constant.HTTP_REQUEST_TIMEOUT)
+				    .build();
+				}
+			}
+		}
+		
+		return _conn;
+	}
+	
 	public String post(String apiURL, String body) {
 //TODO... $apiURL health check MAP and periodical resume polling		
 				
-		String resp = "";
+		String resp = "";		
 		
-		CloseableHttpClient httpClient = null;		
-		
-		try {
-			httpClient =  HttpClients.createDefault();
-			
+		try {		
 			StringEntity body_entity = new StringEntity(body);
 			body_entity.setContentType("application/json");
 			
 			HttpPost post = new HttpPost(apiURL);
-			post.setEntity(body_entity);
+			post.setEntity(body_entity);			
 //TODO... post.setConfig(_reqConfig);
-			
-			StrRespHandler respHd = new StrRespHandler();
-			resp = httpClient.execute(post, respHd);
-			
+									
+			StrRespHandler resHd = new StrRespHandler();
+			resp = _httpClient.execute(post, resHd);
 		} catch (Exception ex) {
-			VEN_LOGGER.error(Utility.stackTrace2string(ex));
-			VEN_LOGGER.error(ex.getMessage());
+			VEN_LOGGER.error("{} on {}",  ex.getMessage(), apiURL);
+			VEN_LOGGER.error(Utility.stackTrace2string(ex));			
 		}
 		finally {
-			try {				
-				if (null != httpClient) httpClient.close();
+			try {
+				if (null != _httpClient) _httpClient.close();
 			} catch (Exception ex) {
-				VEN_LOGGER.error(Utility.stackTrace2string(ex));
-				VEN_LOGGER.error(ex.getMessage());
+				VEN_LOGGER.error("{} on {}",  ex.getMessage(), apiURL);
+				VEN_LOGGER.error(Utility.stackTrace2string(ex));				
 			}				
 		}
 				
@@ -94,16 +123,18 @@ public class APIConnector {
 			int status = response.getStatusLine().getStatusCode();
 			
 			try {
-				if (status >= 200 && status < 300) {
+				
+				if (status >= 200 && status < 300) {					
 					HttpEntity entity = response.getEntity();					
-					if (null != entity) 
-						resp = EntityUtils.toString(entity);															
+					if (null != entity) { 
+						resp = EntityUtils.toString(entity);
+					}
 				} else {
 					VEN_LOGGER.warn("Unexpected response http status code: {}", status);				
 				}
 			} catch (ParseException | IOException ex) {
-				VEN_LOGGER.error(Utility.stackTrace2string(ex));
 				VEN_LOGGER.error(ex.getMessage());
+				VEN_LOGGER.error(Utility.stackTrace2string(ex));				
 			}
 			
 			return resp;
