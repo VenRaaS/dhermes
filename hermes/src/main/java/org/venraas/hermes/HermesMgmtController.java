3015779eeaeeaefed7c82a3e965f0eb29af3b6ab
eric0329 <asdguo@mail2000.com.tw>
2016-08-25 19:31:11 +0800

    [hermes] add register_normal() function

--- hermes/src/main/java/org/venraas/hermes/HermesMgmtController.java
+++ hermes/src/main/java/org/venraas/hermes/HermesMgmtController.java
@@ -1,5 +1,8 @@
 package org.venraas.hermes;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -10,24 +13,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.venraas.hermes.apollo.hermes.ConfManager;
+import org.venraas.hermes.apollo.mappings.EnumParam2recomder;
 import org.venraas.hermes.apollo.raas.CompanyManager;
 import org.venraas.hermes.common.ConstantMsg;
 import org.venraas.hermes.common.EnumOptionBase;
 import org.venraas.hermes.common.EnumResetInterval;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
-/** 
- * usage:
- *     /hermes/mgmt/set_traffic_pct_normal?token=NVHlz4elol1&pct=0.66
- */
 @RestController
 @RequestMapping("/mgmt")
 public class HermesMgmtController {
 	
 	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(HermesMgmtController.class);
 	
+	/** 
+	 * usage:
+	 *     /hermes/mgmt/set_traffic_pct_normal?token=&pct=0.66
+	 */	
 	@CrossOrigin
 	@RequestMapping(value = "/set_traffic_pct_normal", method = RequestMethod.GET)
 	public String set_traffic_pct_normal_GET(String token, double pct) {		
@@ -59,6 +64,10 @@ public class HermesMgmtController {
 		return msg;
 	}
 	
+	/** 
+	 * usage:
+	 *     /hermes/mgmt/set_routing_reset_interval?token=&interval=HOUR
+	 */
 	@CrossOrigin
 	@RequestMapping(value = "/set_routing_reset_interval", method = RequestMethod.GET)
 	public String set_routing_reset_interval_GET(String token, String interval) {		
@@ -102,11 +111,34 @@ public class HermesMgmtController {
 				throw new IllegalArgumentException(msg);
 			}
 			
-			// valid format
-			Gson g = new Gson();
+			//-- input validation
+			Gson g = new Gson();			
+			Type type = new TypeToken<Map<String, Object>>(){}.getType();
+			Map<String, Object> jsonMap = g.fromJson(json, type);
+			
+			// "in_keys2recomder"
+			List<String> inKeys = (List<String>) jsonMap.getOrDefault(EnumParam2recomder.in_keys2recomder.name(), new ArrayList<String>());
+			for (String k : inKeys){
+				String val = (String) jsonMap.getOrDefault(k, "");
+				if (val.isEmpty()) {
+					msg = String.format("Invalid input, key \"%s\" in \"%s\" can't be found in the input Json!", k, EnumParam2recomder.in_keys2recomder.name());
+					throw new IllegalArgumentException(msg);
+				}
+			}
+			
+			// "out_aux_params"
+			List<String> outAuxParams = (List<String>) jsonMap.getOrDefault(EnumParam2recomder.out_aux_params.name(), new ArrayList<String>());
+			for (String k : outAuxParams){
+				String val = (String) jsonMap.getOrDefault(k, "");
+				if (val.isEmpty()) {
+					msg = String.format("Invalid input, key \"%s\" in \"%s\" can't be found in the input Json!", k, EnumParam2recomder.out_aux_params.name());
+					throw new IllegalArgumentException(msg);
+				}
+			}
+			
+			List<String> api_urls = (List<String>) jsonMap.getOrDefault(EnumParam2recomder.api_url.name(), new ArrayList<String>());
 			
 			
-			// valid rec_api
 			
 			// add "group_key", "traffic_type", "availability"
 			
