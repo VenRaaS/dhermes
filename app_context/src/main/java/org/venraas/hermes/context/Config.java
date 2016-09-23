3af42ab3ee9761da35652564f9b0c2b9b1eb3e9d
eric0329 <asdguo@mail2000.com.tw>
2016-09-23 16:34:54 +0800

    [app_context] load configuration from application.properties - apply @PropertySource to load configuration - class Config for storing configuration setting -

---
+++ app_context/src/main/java/org/venraas/hermes/context/Config.java
@@ -0,0 +1,66 @@
+package org.venraas.hermes.context;
+
+
+public class Config {	
+	
+	int conn_timeout;
+	
+	int conn_fail_cond_interval;
+	
+	int conn_fail_cont_count;
+	
+	int conn_fail_resume_interval;
+	
+	String conn_failover_url;
+		
+	static Config _conf = new Config();
+	
+	
+	private Config() { }
+
+	static public Config getInstance() {		
+		return _conf;
+	}
+
+	public int getConn_timeout() {
+		return conn_timeout;
+	}
+	
+	public void setConn_timeout(int ms) {
+		this.conn_timeout = ms;
+	}
+
+	public int getConn_fail_cond_interval() {
+		return conn_fail_cond_interval;
+	}
+
+	public void setConn_fail_cond_interval(int conn_fail_cond_interval) {
+		this.conn_fail_cond_interval = conn_fail_cond_interval;
+	}
+
+	public int getConn_fail_cont_count() {
+		return conn_fail_cont_count;
+	}
+
+	public void setConn_fail_cont_count(int conn_fail_cont_count) {
+		this.conn_fail_cont_count = conn_fail_cont_count;
+	}
+
+	public String getConn_failover_url() {
+		return conn_failover_url;
+	}
+
+	public void setConn_failover_url(String conn_failover_url) {
+		this.conn_failover_url = conn_failover_url;
+	}
+	
+	public int getConn_fail_resume_interval() {
+		return conn_fail_resume_interval;
+	}
+
+	public void setConn_fail_resume_interval(int conn_fail_resume_interval) {
+		this.conn_fail_resume_interval = conn_fail_resume_interval;
+	}
+
+//	
+}
