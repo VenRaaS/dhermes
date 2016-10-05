ef5f6cc21cd3a6cef7ae7a36aa10ba900d4812f6
eric0329 <asdguo@mail2000.com.tw>
2016-10-05 17:35:50 +0800

    [Urility.java] add function duration_sec()

--- common/src/main/java/org/venraas/hermes/common/Utility.java
+++ common/src/main/java/org/venraas/hermes/common/Utility.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,5 +27,9 @@ public class Utility {
     	Date date = new Date();
     	
 		return dateFormat.format(date);		
+	}
+	
+	static public long duration_sec (Date beg, Date end) {
+		return TimeUnit.SECONDS.toSeconds(end.getTime() - beg.getTime());		
 	}
 }
