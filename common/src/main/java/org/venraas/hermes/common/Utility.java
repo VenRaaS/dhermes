8cf5d33a4d5710b716750555053d65e8bd86104d
eric0329 <asdguo@mail2000.com.tw>
2016-11-29 12:09:34 +0800

    [Utility.java] provide a static ListeningExecutorService for cache refresh

--- common/src/main/java/org/venraas/hermes/common/Utility.java
+++ common/src/main/java/org/venraas/hermes/common/Utility.java
@@ -5,13 +5,32 @@ import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Utility {
+	
+	//-- ListenableFuture - https://github.com/google/guava/wiki/ListenableFutureExplained
+	public static final ListeningExecutorService CacheRefreshLES;
+	
 	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(Utility.class);
+	
+	
+	static {
+		ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("cacheRefresherThread - %d").setDaemon(true).build();
+		ExecutorService es = Executors.newSingleThreadExecutor(threadFactory);
+		CacheRefreshLES = MoreExecutors.listeningDecorator(es);
+		VEN_LOGGER.info("cache refresher thread is creaded");
+	}
 
 	static public String stackTrace2string(Exception ex) {
 
